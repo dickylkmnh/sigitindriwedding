a2fcf8a4e4d6fa4ab6848cc5c1b4020687ae8779
dickylkmnh <dickylukmanhakim03@gmail.com>
2025-09-26 02:18:35 +0700

    fix: feedback

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -11,8 +11,9 @@ const sacramento = Sacramento({
 });
 
 export default function Landing() {
-  const audioRef = useRef<HTMLAudioElement>(null);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
   const [playing, setPlaying] = useState(true);
+  const [copied, setCopied] = useState<string | null>(null);
 
   useEffect(() => {
     AOS.init();
@@ -30,6 +31,27 @@ export default function Landing() {
     setPlaying(!playing);
   };
 
+  const copyText = async (id: string, text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setCopied(id);
+      setTimeout(() => setCopied(null), 2000);
+    } catch (err) {
+      // fallback for older browsers
+      const textarea = document.createElement("textarea");
+      textarea.value = text;
+      document.body.appendChild(textarea);
+      textarea.select();
+      try {
+        document.execCommand("copy");
+        setCopied(id);
+        setTimeout(() => setCopied(null), 2000);
+      } finally {
+        document.body.removeChild(textarea);
+      }
+    }
+  };
+
   return (
     <div className="bg-[#800000ae] overflow-x-hidden">
       {/* INVITATION */}
@@ -41,7 +63,7 @@ export default function Landing() {
         <img
           src="/img/gunung_wayang.png"
           alt="Gunungan Wayang"
-          className="absolute inset-0 mx-auto my-auto max-w-full h-auto opacity-20 z-0"
+          className="absolute inset-0 mx-auto my-auto max-w-full max-h-full opacity-20 z-0"
         />
 
         <div
@@ -183,9 +205,7 @@ export default function Landing() {
             className="mt-8 p-6 bg-[#f8f8f84b] rounded-xl shadow text-center"
             data-aos="fade-up"
           >
-            <h4 className="text-xl font-semibold mb-3 text-[#1c1c1c]">
-              Location
-            </h4>
+            <h4 className="text-xl font-semibold mb-3 text-[#1c1c1c]">Location</h4>
             <hr className="my-4 border-[#1c1c1c]" />
             <p className="text-[#1c1c1c] mb-4">
               KP.Sukamaju RT.003 / RW.003 Desa.Ciptaharha Kec.Cipatat
@@ -227,7 +247,7 @@ export default function Landing() {
             </h3>
           </div>
 
-          <div className="grid grid-cols-1">
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
             {/* Foto 1 */}
             <div
               className="overflow-hidden rounded-xl shadow hover:scale-105 transition"
@@ -236,12 +256,172 @@ export default function Landing() {
               <img
                 src="/img/berdua.png"
                 alt="Gallery 1"
-                className="w-full object-cover"
+                className="w-full h-48 object-cover"
+              />
+            </div>
+
+            {/* Foto 2 */}
+            <div
+              className="overflow-hidden rounded-xl shadow hover:scale-105 transition"
+              data-aos="fade-up"
+            >
+              <img
+                src="/img/galery1.jpeg"
+                alt="Gallery 2"
+                className="w-full h-48 object-cover"
+              />
+            </div>
+
+            {/* Foto 3 */}
+            <div
+              className="overflow-hidden rounded-xl shadow hover:scale-105 transition"
+              data-aos="fade-up"
+            >
+              <img
+                src="/img/galery2.jpeg"
+                alt="Gallery 3"
+                className="w-full h-48 object-cover"
+              />
+            </div>
+
+            {/* Foto 4 */}
+            <div
+              className="overflow-hidden rounded-xl shadow hover:scale-105 transition"
+              data-aos="fade-up"
+            >
+              <img
+                src="/img/galery3.jpeg"
+                alt="Gallery 4"
+                className="w-full h-48 object-cover"
+              />
+            </div>
+
+            {/* Foto 5 */}
+            <div
+              className="overflow-hidden rounded-xl shadow hover:scale-105 transition"
+              data-aos="fade-up"
+            >
+              <img
+                src="/img/galery4.jpeg"
+                alt="Gallery 4"
+                className="w-full h-48 object-cover"
+              />
+            </div>
+
+            {/* Foto 6 */}
+            <div
+              className="overflow-hidden rounded-xl shadow hover:scale-105 transition"
+              data-aos="fade-up"
+            >
+              <img
+                src="/img/galery5.jpeg"
+                alt="Gallery 5"
+                className="w-full h-48 object-cover"
               />
             </div>
           </div>
         </div>
       </section>
+
+      {/* GIFT / KADO SECTION */}
+      <section id="gift" className="pt-20">
+        <div className="max-w-4xl mx-auto px-4">
+          <div
+            className="text-center mb-8"
+            data-aos="zoom-in"
+            data-aos-duration="1000"
+          >
+            <h2 className="text-lg text-gray-400">GIFT</h2>
+            <h3
+              className={`text-4xl md:text-5xl font-bold text-[#D4AF37] ${sacramento.className}`}
+            >
+              Kirim Kado
+            </h3>
+            <p className="text-sm text-[#FAF8F5] mt-2">
+              Terima kasih atas doa dan restu yang telah Anda berikan. Bagi yang
+              berkenan mengirimkan tanda kasih, dapat disampaikan melalui cara di
+              bawah ini. Dengan segala kerendahan hati, kami ucapkan terima kasih.
+            </p>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            {/* Card 1 */}
+            <div
+              className="p-6 bg-[#f8f8f84b] rounded-xl shadow hover:shadow-lg transition relative"
+              data-aos="fade-up"
+            >
+              <div className="flex items-center justify-between">
+                <div>
+                  <h4 className="text-lg font-semibold text-[#1c1c1c]">
+                    SIGIT KOMARUDIN
+                  </h4>
+                  <p className="text-sm text-[#1c1c1c]">Bank Mandiri</p>
+                </div>
+                <img
+                  src="/img/mandiri.png"
+                  alt="Logo Mandiri"
+                  className="h-10 w-auto"
+                />
+              </div>
+
+              <div className="mt-12 flex items-center justify-between gap-2">
+                <p className="select-all font-mono text-md text-[#1c1c1c]">
+                  1320028535939
+                </p>
+                <button
+                  onClick={() => copyText("mandiri", "1320028535939")}
+                  className="ml-2 px-3 py-2 bg-[#D4AF37] hover:bg-[#E6BE8A] rounded-md font-semibold text-[#1c1c1c]"
+                >
+                  Salin
+                </button>
+              </div>
+
+              {copied === "mandiri" && (
+                <span className="absolute top-4 right-4 bg-[#1c1c1c] text-white text-xs px-2 py-1 rounded">
+                  Tersalin!
+                </span>
+              )}
+            </div>
+
+            {/* Card 2 */}
+            <div
+              className="p-6 bg-[#f8f8f84b] rounded-xl shadow hover:shadow-lg transition relative"
+              data-aos="fade-up"
+            >
+              <div className="flex items-center justify-between">
+                <div>
+                  <h4 className="text-lg font-semibold text-[#1c1c1c]">DANA</h4>
+                  <p className="text-sm text-[#1c1c1c]">(E-Wallet)</p>
+                </div>
+                <img src="/img/dana.png" alt="Logo Dana" className="h-8 w-auto" />
+              </div>
+
+              <div className="mt-12 flex items-center justify-between gap-2">
+                <p className="select-all font-mono text-md text-[#1c1c1c]">
+                  083116558561
+                </p>
+                <button
+                  onClick={() => copyText("dana", "083116558561")}
+                  className="ml-2 px-3 py-2 bg-[#D4AF37] hover:bg-[#E6BE8A] rounded-md font-semibold text-[#1c1c1c]"
+                >
+                  Salin
+                </button>
+              </div>
+
+              {copied === "dana" && (
+                <span className="absolute top-4 right-4 bg-[#1c1c1c] text-white text-xs px-2 py-1 rounded">
+                  Tersalin!
+                </span>
+              )}
+            </div>
+          </div>
+
+          <p className="text-xs text-center text-[#FAF8F5] mt-4">
+            Terima kasih atas doa dan kebaikan Anda ❤️
+          </p>
+        </div>
+      </section>
+
 
       {/* FOOTER */}
       <section id="footer" className="pt-10 mb-5 text-center text-white">
